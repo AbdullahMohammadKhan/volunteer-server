373c833bd26ab282809dcc3db14d9af59c4e6939
abdullah <hbkamk@gmail.com>
2020-10-20 20:00:14 +0600

    2nd

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 const MongoClient = require("mongodb").MongoClient;
 const uri =
-  "mongodb+srv://abdullah:abdullah@cluster0.rmegi.mongodb.net/volunteer?retryWrites=true&w=majority";
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rmegi.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
   //${process.env.DB_USER}
   //${process.env.DB_PASSWORD}
   //${process.env.DB_NAME}
